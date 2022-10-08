8d62bcce822c5c74a6a9b6bd88b61eabbe970374
Bello Joseph <joexef87@gmail.com>
2022-10-08 03:05:50 +0100

    Update weight.js

--- Weight converter/weight.js
+++ Weight converter/weight.js
@@ -1,142 +1,175 @@
-@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap'); 
+// Global variables declaration
+var userValue = document.getElementById("user-value");
+var selectInput = document.getElementById("input");
+var convertInput = document.getElementById("convert-input");
+var output = document.getElementById("output");
+var convertBtn = document.getElementById("convert-btn");
+var refresh = document.getElementById("refresh");
 
-*{
-box-sizing: border-box;
-font-family:  'Poppins', sans-serif;
-}
-*{
-    box-sizing: border-box;
-}
-html{
-    scroll-behavior: smooth;
-}
-body{
-margin: 0;
-padding: 0;
-width: 100%;
-height: 100%;
-overflow-y:  auto;
-background: #e0e5ec;
-text-align: center;
-}
-.user-inputs{
-    text-align: center;
-    padding: 40px;
-    
-}
-h1{
-    font-size: 1.4rem;
-    color: rgb(58, 85, 237); 
-}
-::placeholder{
-    font-size: 0.8rem;
-    
-}
-.container{
- display: flex;
- align-items: center;
- justify-content: center;
- margin: 100px auto;
- text-align: center;
-}
-select,#items-container,button,#convert-input,#user-value{
-    background-color: #e0e5ec;;
-    box-shadow: 9px 9px 16px rgba(163, 177, 198, 0.9),
-     -9px -9px 16px rgba(255, 255, 255, 0.5);
-     border: 1px solid rgba(214, 214, 214, 0.2); 
-}
-#items-container{
-    width: 450px;
-    height: 550px;
-    border-radius: 12px;
-    border-radius: 20px;
-    text-align: center;
-    position: relative;
-}
-#input{
-    padding: 10px 50px;
-    border-radius: 20px;
-    margin-top: 25px;
-}
-#line{
-    width: 90%;
-    border: 1px solid rgb(118, 117, 117);
-    margin: 50px auto;
-}
-#convert{
-display: flex;
-gap: 2rem;
-align-items: center;
-justify-content: center;
-margin-top: 50px;
-}
-#user-value{
-    margin-top: 50px;
-    background-color: #fff;
-    font-size: 1rem;
-    text-align: center;
-    padding: 11px 0px;
-    outline: none;
-}
-#output{   
-     height: 45px;
-     width: 150px;
-     text-align: center;
-     border-radius: 20px;
-}
-#convert-btn{
-    padding: 15px 30px;
-    position: absolute;
-    left: 70px;
-    border-radius: 20px;
-}
-#convert-btn:hover{
-    background-color: rgb(58, 85, 237);
-    transition: all ease-in-out 0.4s;
-    color: white;
-    transform: scale(1.05);
-}
-#convert-input{
-    position: absolute;
-    padding: 12px 1px;
-    outline: none;
-    margin-top: 2px;
-    background-color: #fff;
-    text-align: center;
-}
-select{
-    outline: none;
-}
-h2{
-    font-size: 1.7rem;
-    margin-top: 1rem;
-    position: relative;
-    top: 2rem;
-}
-
-#refresh{
-    position: absolute;
-    left: 30px;
-    top: 30px;
-    font-size: 1.5rem;
-    color: rgb(40, 64, 205);
+convertBtn.addEventListener("click",calculate);
+convertBtn.addEventListener("click",alertMe);
+ 
+function alertMe(){
+   if(userValue.value === ""){
+      alert("please enter a value on the input field !");
+    }
+   else{
+      convertInput.style.color = "white";
+      convertInput.style.background = "#64b450"
    }
-
-@media screen and (max-width: 460px) {
-    #items-container{
-        width: 90%;
-        margin: 100px auto;
-    }
-    #convert-btn{
-        left: 40px;
-    }
-    #convert-input{
-        max-width: 40%;
-    }
-    @media screen and (max-width: 370px){
-        #items-container{
-        width: 95%;
-        margin: 100px auto;
-     }
-    }
 }
+
+function calculate(){
+    
+   //  check if the value selected to be converted is equal to each other return the input provided
+
+  if(selectInput.value === "Kilograms" && output.value === "Kilograms" ||
+  selectInput.value === "grams" && output.value === "grams" ||
+  selectInput.value === "pounds" && output.value === "pounds" ||  
+  selectInput.value === "ounces" && output.value === "ounces" ||  
+  selectInput.value === "Tonne" && output.value === "Tonne"
+  )
+ {
+   convertInput.value = 
+   userValue.value;
+  }
+
+
+//   Kilograms
+
+else if(selectInput.value === "Kilograms" && output.value === "grams"){
+   convertInput.value = userValue.value * 1000 + "g"
+}
+
+else if(selectInput.value === "Kilograms" && output.value === "pounds"){
+   convertInput.value = userValue.value * 2.205 + "lbs";
+ 
+}
+
+else if(selectInput.value === "Kilograms" && output.value === "ounces"){
+   convertInput.value = userValue.value * 35.274 + "Oz";
+}
+else if(selectInput.value === "Kilograms" && output.value === "Tonne"){
+   convertInput.value = userValue.value / 1000 + "Mg";
+}
+
+// grams
+else if(selectInput.value === "grams" && output.value === "Kilograms"){
+   convertInput.value = userValue.value / 1000 + "Kg"
+}
+
+else if(selectInput.value === "grams" && output.value === "pounds"){
+   convertInput.value = (userValue.value / 453.6).toFixed(4) + "lbs";
+   
+ 
+}
+
+else if(selectInput.value === "grams" && output.value === "ounces"){
+   convertInput.value = (userValue.value / 28.35).toFixed(4) + "Oz";
+}
+else if(selectInput.value === "grams" && output.value === "Tonne"){
+   convertInput.value = userValue.value / 1000000 + "Mg";
+}
+
+// ounces
+
+else if(selectInput.value === "ounces" && output.value === "Kilograms"){
+   convertInput.value = (userValue.value / 35.274).toFixed(4) + "Kg";
+   
+}
+
+else if(selectInput.value === "ounces" && output.value === "pounds"){
+   convertInput.value = userValue.value * 16 + "lbs";
+ 
+}
+
+else if(selectInput.value === "ounces" && output.value === "grams"){
+   convertInput.value = userValue.value * 28.35 + "g";
+}
+else if(selectInput.value === "ounces" && output.value === "Tonne"){
+   convertInput.value = userValue.value * 35270 + "Mg";  
+}
+
+// pounds
+
+
+else if(selectInput.value === "pounds" && output.value === "Kilograms"){
+   convertInput.value = userValue.value * 2.205 + "Kg";
+   
+}
+
+else if(selectInput.value === "pounds" && output.value === "ounces"){
+   convertInput.value = userValue.value / 16 + "Oz";
+ 
+}
+
+else if(selectInput.value === "pounds" && output.value === "grams"){
+   convertInput.value = userValue.value * 453.6 + "g";
+}
+else if(selectInput.value === "pounds" && output.value === "Tonne"){
+   convertInput.value = userValue.value * 2205 + "Mg";  
+}
+
+
+// Tonne
+
+else if(selectInput.value === "Tonne" && output.value === "Kilograms"){
+   convertInput.value = userValue.value * 1000 + "Kg";
+   
+}
+
+else if(selectInput.value === "Tonne" && output.value === "ounces"){
+   convertInput.value = (userValue.value / 3520).toFixed(4) + "Oz";
+ 
+}
+
+else if(selectInput.value === "Tonne" && output.value === "pounds"){
+   convertInput.value = (userValue.value / 2205).toFixed(4) + "lbs";
+}
+
+else if(selectInput.value === "Tonne" && output.value === "grams"){
+   convertInput.value = userValue.value * 1000000 + "g";
+}
+  else return convertInput.value =  "";
+  
+}
+
+// Clear Texts
+
+convertInput.addEventListener("keyup",clearText);
+userValue.addEventListener("keydown",clearText);
+
+
+function clearText(){
+   if(convertInput.value == "" && userValue.value !== ""){
+       userValue.value = "";
+       convertInput.style.color = "#000";
+       convertInput.style.background = "none";
+}
+else {
+   return convertInput.value = "";
+}
+}
+
+//refresh
+
+refresh.addEventListener("click",refreshList);
+function refreshList(e){
+ if(e.target === refresh){
+ userValue.value = "";
+ convertInput.value = "";
+ refresh.style.transform = "rotate(90deg)";
+ convertInput.style.color = "#000";
+ convertInput.style.background = "none";
+ }
+ else{
+   "";
+ }
+}
+
+refresh.addEventListener("mouseleave",rotateBack);
+function rotateBack(){
+  refresh.style.transform = "rotate(-90deg)";
+  refresh.style.transition = "all .2s";
+}
+
